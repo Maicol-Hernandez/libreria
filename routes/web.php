bd24b2259bc2c4f1759f09a25f5ce14db2059e60
Maicol Hernandez Peralta <maicolhernandez420@gmail.com>
2022-11-05 08:42:24 -0500

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\LibroController;
+use App\Http\Controllers\CategoriaController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::resource('libros', LibroController::class)->middleware('auth');
+Route::resource('categorias', CategoriaController::class)->middleware('auth');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
